1807178465c22c854cf8cfa5e7a1ea28c1e8fd66
giacomo3008 <giacomorigo7@gmail.com>
2024-11-29 19:26:06 +0100

    scroll animation 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ document.addEventListener("scroll", () => {
     const header = document.getElementsByTagName("header")[0];
     const hero = document.getElementsByClassName("hero")[0];
 
-    // Controlla la posizione della prima sezione rispetto alla viewport
+    // Controlla la posizione della section hero
     const heroBottom = hero.getBoundingClientRect().bottom;
 
     if (heroBottom <= 50) {
-        // Cambia colore quando si scorre oltre la sezione
+        // Cambia colore quando si scorre oltre hero
         header.style.backgroundColor = "white";
     } else {
-        // Ripristina il colore originale quando si torna indietro
+        // Ripristina il colore originale 
         header.style.backgroundColor = "#ffc017";
     }
 });
